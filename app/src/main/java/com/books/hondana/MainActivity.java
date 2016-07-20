417b090974254ac17a2297441c246ffb7fa9ad5e
小玉沙貴子 <design@spica-travel.com>
2016-07-20 12:27:41 +0900

    main activity

--- app/src/main/java/com/books/hondana/MainActivity.java
+++ app/src/main/java/com/books/hondana/MainActivity.java
@@ -1,5 +1,6 @@
 package com.books.hondana;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
