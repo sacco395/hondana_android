260c3bdb4ae29d4f225a5bb24326c8266e86517d
小玉沙貴子 <design@spica-travel.com>
2016-07-26 12:56:13 +0900

    ツールバーをロゴに

--- app/src/main/java/com/books/hondana/MainActivity.java
+++ app/src/main/java/com/books/hondana/MainActivity.java
@@ -1,7 +1,7 @@
 package com.books.hondana;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
+// import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,27 +15,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-
-import com.books.hondana.databinding.ActivityMainBinding;
-import com.books.hondana.viewmodel.main.ContentViewModel;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    ContentViewModel contentViewModel;
-
-    ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        contentViewModel = new ContentViewModel();
-
-        binding.setContentViewModel(contentViewModel);
-
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -45,25 +32,35 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                //スキャン画面へ移動
-//                Intent intent = new Intent(MainActivity.this, BarcodeScanActivity.class);
-//                startActivity(intent);
-                contentViewModel.setMessage("Fab clicked!");
+               //スキャン画面へ移動
+               Intent intent = new Intent(MainActivity.this, BarcodeScanActivity.class);
+               startActivity(intent);
             }
         });
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, binding.drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        binding.drawerLayout.setDrawerListener(toggle);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+
+        //         this, binding.drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        // binding.drawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
-        binding.navView.setNavigationItemSelectedListener(this);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // binding.navView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START);
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+
+        // if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+        //     binding.drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
