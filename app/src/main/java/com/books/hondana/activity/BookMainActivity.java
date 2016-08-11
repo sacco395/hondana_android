b294fc7974ce8c7b000ae6a32b59b7f9996e45f5
小玉沙貴子 <design@spica-travel.com>
2016-08-11 11:38:33 +0900

    トップ入れ込み力技

--- app/src/main/java/com/books/hondana/activity/BookMainActivity.java
+++ app/src/main/java/com/books/hondana/activity/BookMainActivity.java
@@ -20,30 +20,44 @@
 package com.books.hondana.activity;
 
 import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.books.hondana.BookMainFragmentPagerAdapter;
-import com.books.hondana.HondanaBooksFragment;
-import com.books.hondana.R;
 import com.books.hondana.Connection.QueryParamSet;
+import com.books.hondana.HondanaBooksFragment;
 import com.books.hondana.Model.KiiBook;
+import com.books.hondana.R;
 
 public class BookMainActivity extends AppCompatActivity
-implements HondanaBooksFragment.OnFragmentInteractionListener {
+implements HondanaBooksFragment.OnFragmentInteractionListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
     //private static final String TAG = "BookMainActivity";
     final static String TAG = BookMainActivity.class.getSimpleName();
@@ -79,45 +93,129 @@ implements HondanaBooksFragment.OnFragmentInteractionListener {
 
     private ViewPager viewPager;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.book_main_frag);
 
-        //        // BarCodeScanner起動
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        getWindow().setExitTransition(new Slide());
+
+        setContentView(R.layout.activity_book_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         mFab = (FloatingActionButton) findViewById(R.id.fab);
         mFab.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  launchSimpleActivity(v);
-              }
-        });
-
-        // 戻るボタン
-        btnReturn = (Button)findViewById(R.id.btnReturn);
-        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                finish();// 戻る
+                launchSimpleActivity(v);
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                setProfileInMenu(drawerView);
+            }
+        };
+        drawer.setDrawerListener(toggle);
+
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+//        // 戻るボタン
+//        btnReturn = (Button)findViewById(R.id.btnReturn);
+//        btnReturn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                finish();// 戻る
+//            }
+//        });
 
         viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter( new BookMainFragmentPagerAdapter( getSupportFragmentManager()));
 
     }
 
-    // the user can add items from the options menu.
-    // create that menu here - from the res/menu/menu.xml file
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+
+            // if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            //     binding.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_top, menu);
         return true;
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //BookMainActivity.this.addItem(null);
-        return true;
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_search:{
+                Intent intent = new Intent(this, SearchActivity.class);
+                startActivity(intent,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+            }
+            break;
+            case R.id.nav_notifications: {
+                Intent intent = new Intent(this, PassedActivity.class);
+                startActivity(intent,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+
+                break;
+            }
+            case R.id.nav_todo:{
+                Intent intent = new Intent(this, EvaluateActivity.class);
+                startActivity(intent,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+
+                break;
+            }
+        }
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
     }
+
+    // the user can add items from the options menu.
+    // create that menu here - from the res/menu/menu.xml file
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        super.onCreateOptionsMenu(menu);
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        //BookMainActivity.this.addItem(null);
+//        return true;
+//    }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //  Barcode Scanner 起動関連
@@ -208,5 +306,65 @@ implements HondanaBooksFragment.OnFragmentInteractionListener {
     @Override
     public void onFragmentInteraction(Uri uri) {
         ;
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_like) {
+            Intent intent = new Intent(this, LikesActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_exchange) {
+            Intent intent = new Intent(this, SwapBookActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_transaction) {
+            Intent intent = new Intent(this, RequestActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_set) {
+            Intent intent = new Intent(this, SetActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_guide) {
+            Intent intent = new Intent(this, GuideActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_mail) {
+            Intent intent = new Intent(this, InquiryActivity.class);
+            startActivity(intent);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    void setProfileInMenu(View drawerView) {
+//        tvUserName.setText(user.getName());
+//        Picasso.with(this)
+//                .load(user.getIconUrl())
+//                .into(ivUserIcon);
+        LinearLayout llUserContainer = (LinearLayout) drawerView.findViewById(R.id.ll_user_container);
+        TextView tvUserName = (TextView) drawerView.findViewById(R.id.tv_user_name);
+        ImageView ivUserIcon = (ImageView) drawerView.findViewById(R.id.iv_user_icon);
+
+        llUserContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick");
+                Intent intent = new Intent(BookMainActivity.this, UserpageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
