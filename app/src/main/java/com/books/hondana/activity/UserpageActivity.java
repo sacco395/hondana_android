8b7473482405da2b1bec655242b4be3b484d5046
小玉沙貴子 <design@spica-travel.com>
2016-09-09 01:02:41 +0900

    途中

--- app/src/main/java/com/books/hondana/activity/UserpageActivity.java
+++ app/src/main/java/com/books/hondana/activity/UserpageActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,19 +25,29 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.books.hondana.Connection.KiiCloudConnection;
 import com.books.hondana.Connection.QueryParamSet;
 import com.books.hondana.Model.KiiBook;
+import com.books.hondana.Model.KiiCloudBucket;
+import com.books.hondana.Model.Member;
 import com.books.hondana.PRBookListViewAdapter;
 import com.books.hondana.R;
 import com.books.hondana.util.LogUtil;
+import com.kii.cloud.storage.KiiObject;
 import com.kii.cloud.storage.KiiUser;
+import com.kii.cloud.storage.query.KiiQueryResult;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class UserpageActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         AdapterView.OnItemClickListener {
 
     private static final String TAG = "UserpageActivity";
+
+    final ImageLoader imageLoader = ImageLoader.getInstance();
 
     // Intent Parameter
     private static final int ACT_READ_BARCODE = 1;
@@ -77,6 +88,7 @@ public class UserpageActivity extends AppCompatActivity
             R.drawable.october,
             R.drawable.muji,
     };
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,8 +137,8 @@ public class UserpageActivity extends AppCompatActivity
 
         //navigationViewにアイコンここから
         View header = navigationView.getHeaderView(0);
-        ImageView userIcon = (ImageView) header.findViewById(R.id.iv_user_icon);
-        Picasso.with(this).load("http://www.flamme.co.jp/common/profile/kasumi_arimura.jpg").into(userIcon);
+        ImageView userIcon2 = (ImageView) header.findViewById(R.id.iv_user_icon);
+        Picasso.with(this).load("http://www.flamme.co.jp/common/profile/kasumi_arimura.jpg").into(userIcon2);
         header.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -175,10 +187,34 @@ public class UserpageActivity extends AppCompatActivity
             }
         });
 
-        ImageView userIcon2 = (ImageView)findViewById(R.id.user_icon);
-        Picasso.with(this).load("http://www.flamme.co.jp/common/profile/kasumi_arimura.jpg").into(userIcon2);
-        // binding.navView.setNavigationItemSelectedListener(this);
+//        ImageView userIcon2 = (ImageView)findViewById(R.id.user_icon);
+//        Picasso.with(this).load("http://www.flamme.co.jp/common/profile/kasumi_arimura.jpg").into(userIcon2);
+//        // binding.navView.setNavigationItemSelectedListener(this);
 
+        final ImageView userIcon = (ImageView)findViewById(R.id.user_icon);
+
+        final KiiCloudConnection membersConnection = new KiiCloudConnection(KiiCloudBucket.MEMBERS);
+        membersConnection.loadMember(userId, new KiiCloudConnection.SearchFinishListener() {
+            @Override
+            public void didFinish(int token, KiiQueryResult<KiiObject> result, Exception e) {
+                Log.d(TAG, "didFinish(result: " + result + ")");
+                if (result == null) {
+                    Log.w(TAG, e);
+                    return;
+                }
+
+                final List<KiiObject> kiiObjects = result.getResult();
+                Log.d(TAG, "members.size: " + kiiObjects.size());
+                if (kiiObjects != null && kiiObjects.size() > 0) {
+                    final KiiObject kiiObject = kiiObjects.get(0);// ひとつしか来ていないはずなので0番目だけ使う
+                    final Member member = new Member(kiiObject);
+
+                    final String imageUrl = member.get(Member.IMAGE_URL);
+                    Log.d(TAG, "imageUrl: " + imageUrl);
+                    imageLoader.displayImage(imageUrl, userIcon);
+                }
+            }
+        });
 
 
         // ListViewのインスタンスを生成
